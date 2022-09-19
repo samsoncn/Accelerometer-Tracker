f3a0a2af3a2e69ae1777e5584f6c74b64e215303
samsoncn <samsonc0514@gmail.com>
2022-09-19 09:46:38 +0800

    Initial Commit

---
+++ Accelerometer Tracker/Accelerometer_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accelerometer_TrackerApp.swift
+//  Accelerometer Tracker
+//
+//  Created by Samson Chan on 2022-09-19.
+//
+
+import SwiftUI
+
+@main
+struct Accelerometer_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
